0e04e998867394365c52ec7e0a2c6539ba90a788
yunusemrebitkay <73989510+yunusemrebitkay@users.noreply.github.com>
2020-11-19 22:00:31 +0300

    Update FiyatÖlçme.cs

--- FiyatÖlçme.cs
+++ FiyatÖlçme.cs
@@ -1,10 +1,6 @@
-﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.ComTypes;
-using System.Security.Cryptography.X509Certificates;
 
 namespace Algoritma_Ders_Pratik
 {
